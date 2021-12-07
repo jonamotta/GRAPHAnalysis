a9351bc345e1ad8c46f7cf4e669ae1241e8a9277
Jona Motta <jona.motta@polytechnique.edu>
2021-12-07 13:02:30 +0100

    update T3 submitter

--- scripts/skimUtils/makeListOnStorage.py
+++ scripts/skimUtils/makeListOnStorage.py
@@ -4,55 +4,32 @@ import datetime
 
 # ====================================================================
 
-# tag = "/GluGluToHHTo2B2Tau_node_SM_PU200"
-# tag = "/GluGluToHHTo2B2Tau_node_SM_PU200_caloTruth"
-# tag = "/GluGluToHHTo2B2Tau_PU200_4iso"
 
-# tag = "/RelValTenTau_PU200_caloTruth"
-# tag = "/RelValTenTau_noPU_caloTruth"
-# tag = "/RelValTenTau_PU200_4iso"
-# tag = "/RelValTenTau_noPU_4iso"
+tag1 = "/GluGluToHHTo2B2Tau_v11_1_PU200"
+tag2 = "/RelValTenTau_v11_1_PU200"
+tag3 = "/RelValSingleTau_v11_1_PU200"
+tag4 = "/RelValNu_v11_1_PU200"
+tag5 = "/Minbias_v11_1_PU200"
+tag6 = "/RelValQCD_v11_1_PU200"
+tag7 = "/VBFHToTauTau_v11_1_PU200"
+tag8 = "/VBFHToInv_v11_1_PU200"
+tag9 = "/ZprimeToTauTau_v11_1_PU200"
 
-# tag = "/RelValSingleTau_PU200_4iso"
-# tag = "/RelValSingleTau_PU200_caloTruth"
-# tag = "/RelValSingleTau_noPU_4iso"
-# tag = "/RelValSingleTau_noPU_caloTruth"
-
-# tag = "/RelValNu_PU200"
-# tag = "/Minibias_PU200"
-
-# tag = "/QCD_PU200_4iso"
-# tag = "/QCD_noPU_4iso"
-# tag = "/RelValQCD_PU200_4iso"
-# tag = "/RelValQCD_noPU_4iso"
-
-# tag = "/TT_noPU"
-# tag = "/TT_PU200"
-
-#tag="/VBFHToTauTau_PU200"
-
-tag="/ZprimeTauTau_PU200"
-
+tags = [tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8, tag9]
 
 # ====================================================================
 
-# outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/GluGluToHHTo2B2Tau_v11'
+outFolder1 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/GluGluToHHTo2B2Tau_v11_1'
+outFolder2 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValTenTau_v11_1'
+outFolder3 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValSingleTau_v11_1'
+outFolder4 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValNu_v11_1'
+outFolder5 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/Minbias_v11_1'
+outFolder6 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValQCD_v11_1'
+outFolder7 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/VBFHToTauTau_v11_1'
+outFolder8 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/VBFHToInv_v11_1'
+outFolder9 = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/ZprimeToTauTau_v11_1'
 
-# outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValTenTau_v11'
-# outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValSingleTau_v11'
-
-# outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValNu_v11'
-
-# outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/QCD_v11'
-
-# outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/Minbias_v11'
-
-#outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/RelValTT_v11'
-
-#outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/VBFToTauTau_v11'
-
-outFolder = '/home/llr/cms/motta/HGCAL/CMSSW_11_1_0/src/GRAPHAnalysis/inputFiles/ZprimeToTauTau_v11'
-
+outFolders = [outFolder1, outFolder2, outFolder3, outFolder4, outFolder5, outFolder6, outFolder7, outFolder8, outFolder9]
 
 # ====================================================================
 
@@ -73,46 +50,52 @@ def saveToFile (lista, filename):
 
 os.system('mkdir -p '+outFolder)
 
-useOnly = [] #empty list to do list fot all the folders
+useOnly = [] #empty list to do list for all the folders
 
 dpmHome = '/dpm/in2p3.fr/home/cms/trivcat'
-# partialPath = "/store/user/jmotta/HGCAL/GluGluToHHTo2B2Tau_node_SM_14TeV-madgraph-pythia8_tuneCP5"
-# partialPath = "/store/user/jmotta/HGCAL/RelValNuGun"
-# partialPath = '/store/user/jmotta/HGCAL/RelValTenTau_15_500'
-# partialPath = '/store/user/jmotta/HGCAL/RelValSingleTauFlatPt2To150'
-# partialPath = "/store/user/jmotta/HGCAL/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8"
-# partialPath = "/store/user/jmotta/HGCAL/RelValQCD_FlatPt_15_3000HS_14"
-# partialPath = "/store/user/jmotta/HGCAL/RelValQCD_Pt15To7000_Flat_14"
-# partialPath = "/store/user/jmotta/HGCAL/MinBias_TuneCP5_14TeV-pythia8"
-# partialPath = "/store/user/jmotta/HGCAL/RelValTTbar_14TeV"
-# partialPath = "/store/user/jmotta/HGCAL/VBFHToTauTau_M125_TuneCUETP8M1_14TeV_powheg_pythia8"
-partialPath = "/store/user/jmotta/HGCAL/ZprimeToTauTau_M-500_TuneCP5_14TeV-pythia8-tauola"
+partialPath1 = "/store/user/jmotta/HGCAL/GluGluToHHTo2B2Tau_node_SM_14TeV-madgraph-pythia8_tuneCP5"
+partialPath2 = '/store/user/jmotta/HGCAL/RelValTenTau_15_500'
+partialPath3 = '/store/user/jmotta/HGCAL/RelValSingleTauFlatPt2To150'
+partialPath4 = "/store/user/jmotta/HGCAL/RelValNuGun"
+partialPath5 = "/store/user/jmotta/HGCAL/MinBias_TuneCP5_14TeV-pythia8"
+partialPath6 = "/store/user/jmotta/HGCAL/RelValQCD_FlatPt_15_3000HS_14"
+partialPath7 = "/store/user/jmotta/HGCAL/VBFHToTauTau_M125_TuneCUETP8M1_14TeV_powheg_pythia8"
+partialPath8 = "/store/user/jmotta/HGCAL/VBF_HToInvisible_M125_14TeV_powheg_pythia8_TuneCP5"
+partialPath9 = "/store/user/jmotta/HGCAL/ZprimeToTauTau_M-500_TuneCP5_14TeV-pythia8-tauola"
 
-path = dpmHome + partialPath + tag
-if outFolder[-1] != "/": outFolder += '/'
+partialPaths = [partialPath1, partialPath2, partialPath3, partialPath4, partialPath5, partialPath6, partialPath7, partialPath8, partialPath9]
 
-getDateTimeFolder = "/usr/bin/rfdir %s | awk '{print $9}'" % path
-pipeDateTimeFolder = Popen(getDateTimeFolder, shell=True, stdout=PIPE)
+for i in range(len(partialPaths))
+    partialPath = partialPaths[i]
+    tag = tags[i]
+    outFolder = outFolders[i]
+    os.system('mkdir -p '+outFolder)
+    
+    path = dpmHome + partialPath + tag
+    if outFolder[-1] != "/": outFolder += '/'
 
-allLists = {} #dictionary
+    getDateTimeFolder = "/usr/bin/rfdir %s | awk '{print $9}'" % path
+    pipeDateTimeFolder = Popen(getDateTimeFolder, shell=True, stdout=PIPE)
 
-for line in pipeDateTimeFolder.stdout:
-    if useOnly:
-        if not (line.strip()) in useOnly:
-            continue
+    allLists = {} #dictionary
 
-    samplesPath = (path + '/' + line.strip() + '/0000').strip() 
-    sampleDateTime = line.strip()
-    allLists[sampleDateTime] = []
+    for line in pipeDateTimeFolder.stdout:
+        if useOnly:
+            if not (line.strip()) in useOnly:
+                continue
 
-    getFiles = "/usr/bin/rfdir %s | awk '{print $9}'" % samplesPath
-    pipeFiles = Popen(getFiles, shell=True, stdout=PIPE)
+        samplesPath = (path + '/' + line.strip() + '/0000').strip() 
+        sampleDateTime = line.strip()
+        allLists[sampleDateTime] = []
 
-    for file in pipeFiles.stdout:
-        filePath = (samplesPath + '/' + file).strip()
-        allLists[sampleDateTime].append(formatName(filePath,dpmHome))
+        getFiles = "/usr/bin/rfdir %s | awk '{print $9}'" % samplesPath
+        pipeFiles = Popen(getFiles, shell=True, stdout=PIPE)
 
-for sample, lista in allLists.iteritems():
-   if lista: 
-      outName = outFolder + tag + '_' + sample.split('_')[0] + ".txt"
-      saveToFile (lista, outName)
+        for file in pipeFiles.stdout:
+            filePath = (samplesPath + '/' + file).strip()
+            allLists[sampleDateTime].append(formatName(filePath,dpmHome))
+
+    for sample, lista in allLists.iteritems():
+       if lista: 
+          outName = outFolder + tag + '_' + sample.split('_')[0] + ".txt"
+          saveToFile (lista, outName)
